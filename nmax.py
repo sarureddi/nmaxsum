1d969ae3572a4f85b38dfa92687632a0670fff3d
saru <sarumathiayyappan@gmail.com>
2019-06-07 12:39:27 +0530

    Create nmax.py

---
+++ nmax.py
@@ -0,0 +1,17 @@
+n1=int(input())
+a1=list(map(int,input().split()))
+b1=[]
+c1=[]
+for i1 in range(len(a1)):
+    if(i1%2==0):
+        b1.append(a1[i1])
+    else:
+        c1.append(a1[i1])
+for j1 in b1:
+    d1=sum(b1)
+for k1 in c1:
+    f1=sum(c1)
+if(d1>f1):
+    print(d1)
+else:
+    print(f1)
